3ae5f1afac89e8f62434c7cf6891c4a97cd824ff
Thomas Ellis <thomas.ellis@gmi.oeaw.ac.at>
2021-06-15 14:30:00 +0200

    Merge NovaSeq bam files split over zip files
    I learned that bisulphite data from the NovaSeq machine (currently plates 167,
    168 and 169) come as two zip files, with reads for each sample split across
    two zip files. Added 003.scripts/001.unzip_raw_bams.sh to merge these with
    samtools before running pipelines. Update names of other scripts with new
    numbers accordingly. I removed the unzipping commands from the methylseq
    script.
    
    Removed folders for scripts to run each pipeline on plates separately.
    
    Added script to link up sequence names with the field master list. This isn't
    fully functional yet because of the issue with reads being split across files.
    
     On branch process_reads
     Changes to be committed:
    	new file:   003.scripts/001.unzip_raw_bams.sh
    	deleted:    003.scripts/002.allc/allc_144.sh
    	renamed:    003.scripts/001.methylseq.sh -> 003.scripts/002.methylseq.sh
    	deleted:    003.scripts/003.genotype_calls/calls_144.sh
    	renamed:    003.scripts/002.snpmatch.sh -> 003.scripts/003.snpmatch.sh
    	deleted:    003.scripts/004.snpmatch/match_144.sh

--- 003.scripts/002.methylseq.sh
+++ 003.scripts/002.methylseq.sh
@@ -9,45 +9,21 @@ DIR=/scratch-cbe/users/thomas.ellis
 # Where the data are
 # Reference genome
 ref_genome=$PROJ/001.data/001.raw/TAIR10_wholeGenome.fasta
-# Location of the raw zip file on the VBC cluster
-RAW=$PROJ/001.data/001.raw/001.raw_reads
+DATA=$DIR/data # where unzipped reads are saved
 
 # Where to save the output
-DATA=$DIR/data # where to unzip raw reads
 WORK=$DIR/000.work/001.methylseq # Nextflow working directory
 MSEQ=$DIR/001.methylseq # where to output results of the pipeline
 OUT=$PROJ/001.data/002.processed # where to copy the data when finished
 
-mkdir -p $DATA
 mkdir -p $MSEQ
 mkdir -p $WORK
 mkdir -p $OUT
 
-# Unzip raw data
-# Plate 144
-unzip $RAW/CDN24ANXX_2_M8584.zip -d $DATA
-unzip $RAW/CDN24ANXX_3_M8585.zip -d $DATA
-unzip $RAW/CDN24ANXX_4_M8587.zip -d $DATA
-unzip $RAW/CDN24ANXX_5_M8588.zip -d $DATA
-# Plate 145
-unzip $RAW/CDN2BANXX_1_M8246.zip -d $DATA
-unzip $RAW/CDN2BANXX_4.zip -d $DATA
-unzip $RAW/CDN2BANXX_5.zip -d $DATA
-unzip $RAW/CDN2BANXX_6.zip -d $DATA
-# Plate 167
-zipfile=$RAW/elxRjzjh9h-HKKWJDRXX_20200415B_demux_2_R9456.zip
-unzip $zipfile -d $DATA
-# Plate 168
-# zipfile=$RAW/anQaf5Iadx-HMYF5DRXX_20200928B_demux_1_R10191.zip
-# unzip $zipfile -d $DATA
-# Plate 169
-zipfile=$RAW/Tjpak3IDx6-HMYF5DRXX_20200928B_demux_2_R10191.zip
-unzip $zipfile -d $DATA
-
 # Run nextflow
 nextflow run ~/methylseq/main.nf \
 -profile cbe,singularity \
---input "$DATA/[CH]*/*.bam" \
+--input "$DATA/plate*/*.bam" \
 --fasta $ref_genome \
 --outdir $MSEQ \
 --umeth "ChrC:" \
